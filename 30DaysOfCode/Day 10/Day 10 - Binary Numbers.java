ed72efc4433292c59975724186eb69f9f20fa9dd
niklasweimann <nikweimann@gmail.com>
2017-07-31 18:45:26 +0200

    Added Day 10&9 of 30 Days of Code and Datebase\Relational Algebra Challenges

---
+++ 30DaysOfCode/Day 10/Day 10 - Binary Numbers.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+
+		int n = s.nextInt();
+
+		String binary = Integer.toBinaryString(n);
+		int maxConsecutiveOneNum = 0;
+		int consecutiveOneNum = 0;
+		for (int i = 0; i < binary.length(); i++) 
+        {
+			if (binary.charAt(i) == '1') 
+            {
+				consecutiveOneNum++;
+                if(consecutiveOneNum > maxConsecutiveOneNum)
+                    maxConsecutiveOneNum = consecutiveOneNum;
+			} else 
+            {
+				consecutiveOneNum = 0;
+			}
+		}
+		System.out.println(maxConsecutiveOneNum);
+
+		s.close();
+    }
+}
