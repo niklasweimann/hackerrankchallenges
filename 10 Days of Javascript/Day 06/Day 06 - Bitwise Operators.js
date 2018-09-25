878fdf19a5af4ad6767b82703010dec0ba6a2aa3
Niklas Weimann <nikweimann@gmail.com>
2018-09-25 17:54:18 +0200

    Updating Day 06 and Adding Day 07

---
+++ 10 Days of Javascript/Day 06/Day 06 - Bitwise Operators.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k) {
+    var Solution = [];
+    for (let a = 1; a < k; a++) {
+        for (let b = a + 1; b <= n; b++) {
+            if ((a < b) && ((a & b) < k)) {
+                Solution.push(a & b);
+            }
+        }
+    }
+    return Math.max.apply(undefined, Solution);
+}
+function main() {
+    const q = +(readLine());
+
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+
+        console.log(getMaxLessThanK(n, k));
+    }
+}
